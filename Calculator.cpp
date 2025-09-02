6c6b7040beb6e3b74acdb69d8a711403a0ad8fa2
Parth <parthtalmale@gmail.com>
2025-09-02 16:20:19 +0530

    multiply added

--- Calculator.cpp
+++ Calculator.cpp
@@ -20,6 +20,10 @@ int main()
         int res = num1  - num2;
         cout<<"Subtraction Result: "<<res<<endl;
         break;
+        case 3:
+        int res = num1  * num2;
+        cout<<"Multiplication Result: "<<res<<endl;
+        break;
     }
 
     return 0;
