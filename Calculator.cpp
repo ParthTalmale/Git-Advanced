9232d022c7a64f5953a7908eceb8cd68fb188255
Parth <parthtalmale@gmail.com>
2025-09-02 15:35:22 +0530

    calc add

--- Calculator.cpp
+++ Calculator.cpp
@@ -2,9 +2,21 @@
 
 using namespace std;
 
+
 int main()
 {
-
+    int num1,num2;
+    cout<<"Calculator Options: "<< endl;
+    cout<<"1.Add"<<endl;
+    int choice;
+    cin>>choice;
+    
+    switch(choice){
+        case 1:
+        int res = num1  + num2;
+        cout<<"Addition Result: "<<res<<endl;
+        break;
+    }
 
     return 0;
 }
